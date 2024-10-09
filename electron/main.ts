d7c1b46b5a03a405029081a26085c2ec9f3bb34e
Fabian Klaffenböck <fabian@klaffenboeck.eu>
2024-10-09 17:42:11 +0200

    reading can bus worked

--- electron/main.ts
+++ electron/main.ts
@@ -1,6 +1,5 @@
-import { app, BrowserWindow } from 'electron'
-// import { createRequire } from 'node:module'
-import { fileURLToPath } from 'node:url'
+import {app, BrowserWindow} from 'electron'
+import {fileURLToPath} from 'node:url'
 import path from 'node:path'
 import {autoDetect} from "@fklab/candongle-kvaser";
 
@@ -28,45 +27,64 @@ process.env.VITE_PUBLIC = VITE_DEV_SERVER_URL ? path.join(process.env.APP_ROOT, 
 let win: BrowserWindow | null
 
 function createWindow() {
-  win = new BrowserWindow({
-    icon: path.join(process.env.VITE_PUBLIC, 'electron-vite.svg'),
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.mjs'),
-    },
-  })
+    win = new BrowserWindow({
+        icon: path.join(process.env.VITE_PUBLIC, 'electron-vite.svg'),
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.mjs'),
+        },
+    })
 
-  // Test active push message to Renderer-process.
-  win.webContents.on('did-finish-load', () => {
-    win?.webContents.send('main-process-message', (new Date).toLocaleString())
-  })
+    // Test active push message to Renderer-process.
+    win.webContents.on('did-finish-load', () => {
+        win?.webContents.send('main-process-message', (new Date).toLocaleString())
+    })
 
-  if (VITE_DEV_SERVER_URL) {
-    win.loadURL(VITE_DEV_SERVER_URL)
-  } else {
-    // win.loadFile('dist/index.html')
-    win.loadFile(path.join(RENDERER_DIST, 'index.html'))
-  }
+    if (VITE_DEV_SERVER_URL) {
+        win.loadURL(VITE_DEV_SERVER_URL)
+    } else {
+        // win.loadFile('dist/index.html')
+        win.loadFile(path.join(RENDERER_DIST, 'index.html'))
+    }
 }
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-    win = null
-  }
+    if (process.platform !== 'darwin') {
+        app.quit()
+        win = null
+    }
 })
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
 })
 
+function callback(id: number, data: number[], length: number) {
+    console.log(id)
+    console.log(data)
+    console.log(length)
+}
+
 app.whenReady().then(() => {
-  createWindow()
-  autoDetect().list().then(it => console.log(it))
+    createWindow()
+
+    autoDetect().list().then(it => console.log(it))
+
+    autoDetect().open({path: 1, baudRate: 2500000}).then(dongle => {
+        // const list: CanMessage[] = []
+        //
+        // for (let i = 0; i < 5; i++) {
+        //     list.push({id: 0x10, dlc: 8, data: [1,2,3,4,5,6,7,8]})
+        // }
+
+        dongle.setMessageCallback(callback)
+
+        // dongle.write(list)
+    })
 })
