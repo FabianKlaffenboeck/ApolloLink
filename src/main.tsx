e1df74dfd8c19371e9acefd4cd52d96de536227a
Fabian Klaffenböck <fabian@klaffenboeck.eu>
2024-10-09 21:17:43 +0200

    grid somewhat working

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -11,5 +11,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 
 // Use contextBridge
 window.ipcRenderer.on('main-process-message', (_event, message) => {
-  console.log(message)
+    console.log(message)
 })
+
