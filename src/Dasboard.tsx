22fd887019b0e3b5a88f20e22b1f6e557a135d96
FabianKla <fabian@klaffenboeck.eu>
2024-10-14 19:00:56 +0200

    darkmode working

--- src/Dasboard.tsx
+++ src/Dasboard.tsx
@@ -27,6 +27,7 @@ import {Label} from "@/components/ui/label"
 import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue,} from "@/components/ui/select"
 import {Textarea} from "@/components/ui/textarea"
 import {Tooltip, TooltipContent, TooltipTrigger,} from "@/components/ui/tooltip"
+import {ModeToggle} from "@/components/mode-toggle.tsx";
 
 export const description = "An AI playground with a sidebar navigation and a main content area. The playground has a header with a settings drawer and a share button. The sidebar has navigation links and a user menu. The main content area shows a form to configure the model and messages."
 
@@ -278,6 +279,7 @@ export function Dashboard() {
                         <Share className="size-3.5"/>
                         Share
                     </Button>
+                    <ModeToggle></ModeToggle>
                 </header>
                 <main className="grid flex-1 gap-4 overflow-auto p-4 md:grid-cols-2 lg:grid-cols-3">
                     <div
--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import {Dashboard} from "@/Dasboard.tsx";
 import {TooltipProvider} from "@/components/ui/tooltip.tsx";
+import {ThemeProvider} from "@/components/theme-provider.tsx";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <TooltipProvider>
-            <Dashboard></Dashboard>
-        </TooltipProvider>
+        <ThemeProvider>
+            <TooltipProvider>
+                <Dashboard/>
+            </TooltipProvider>
+        </ThemeProvider>
     </React.StrictMode>,
 )
 
