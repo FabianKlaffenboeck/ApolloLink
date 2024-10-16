487da5352f90f02f1ce24a7268b61171f5c42209
Fabian Klaffenböck <fabian@klaffenboeck.eu>
2024-10-16 14:45:39 +0200

    added dynamic logo

--- src/Dasboard.tsx
+++ src/Dasboard.tsx
@@ -1,153 +1,81 @@
-import {Book, Bot, Code2, LifeBuoy, Settings2, SquareTerminal, SquareUser, Triangle,} from "lucide-react"
+import {LifeBuoy, SquareTerminal,} from "lucide-react"
 import {Button} from "@/components/ui/button"
-import {Tooltip, TooltipContent, TooltipTrigger,} from "@/components/ui/tooltip"
+import {Tooltip, TooltipContent, TooltipProvider, TooltipTrigger,} from "@/components/ui/tooltip"
 import {ModeToggle} from "@/components/mode-toggle.tsx";
+import LogoFKLab_Light from '../public/LogoFKLab_Light.svg'
+import LogoFKLab_Dark from '../public/LogoFKLab_Dark.svg'
+import {Theme, useTheme} from "@/components/theme-provider.tsx";
+import './Dasboard.css'
 
 export function Dashboard() {
+
+    const choseLogoIcon = (themeProviderState: Theme) => {
+        if (themeProviderState == "light") {
+            return <img style={{width: 30.5}} src={LogoFKLab_Dark} alt={""}/>
+        }else {
+            return <img style={{width: 30.5}} src={LogoFKLab_Light} alt={""}/>
+        }
+    }
+
+
     return (
-        <div className="grid h-screen w-full pl-[53px]">
-            <aside className="inset-y fixed  left-0 z-20 flex h-full flex-col border-r">
-                <div className="border-b p-2">
-                    <Button variant="outline" size="icon" aria-label="Home">
-                        <Triangle className="size-5 fill-foreground"/>
-                    </Button>
+        <TooltipProvider>
+            <div className="grid h-screen w-full pl-[53px]">
+                <aside className="inset-y fixed  left-0 z-20 flex h-full flex-col border-r">
+                    <div className="border-b p-2">
+                        {choseLogoIcon(useTheme().theme)}
+                    </div>
+                    <nav className="grid gap-1 p-2">
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <Button
+                                    variant="ghost"
+                                    size="icon"
+                                    className="rounded-lg bg-muted"
+                                    aria-label="Playground"
+                                >
+                                    <SquareTerminal className="size-5"/>
+                                </Button>
+                            </TooltipTrigger>
+                            <TooltipContent side="right" sideOffset={5}>
+                                Playground
+                            </TooltipContent>
+                        </Tooltip>
+                    </nav>
+                    <nav className="mt-auto grid gap-1 p-2">
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <Button
+                                    variant="ghost"
+                                    size="icon"
+                                    className="mt-auto rounded-lg"
+                                    aria-label="Help"
+                                >
+                                    <LifeBuoy className="size-5"/>
+                                </Button>
+                            </TooltipTrigger>
+                            <TooltipContent side="right" sideOffset={5}>
+                                Help
+                            </TooltipContent>
+                        </Tooltip>
+                    </nav>
+                </aside>
+
+                <div className="flex flex-col">
+                    <header className="sticky top-0 z-10 flex h-[53px] items-center gap-1 border-b bg-background px-4">
+                        <h1 className="text-xl font-semibold">ApolloLink</h1>
+                        <div className="ml-auto gap-1.5 text-sm">
+                            <ModeToggle></ModeToggle>
+                        </div>
+                    </header>
+
+                    <main className="grid flex-1 gap-4 overflow-auto p-4 grid-cols-3 ">
+                        <div
+                            className="relative flex h-full min-h-[50vh] flex-col rounded-xl bg-muted/50 p-4 col-span-3">
+                        </div>
+                    </main>
                 </div>
-
-                <nav className="grid gap-1 p-2">
-                    <Tooltip>
-                        <TooltipTrigger asChild>
-                            <Button
-                                variant="ghost"
-                                size="icon"
-                                className="rounded-lg bg-muted"
-                                aria-label="Playground"
-                            >
-                                <SquareTerminal className="size-5"/>
-                            </Button>
-                        </TooltipTrigger>
-                        <TooltipContent side="right" sideOffset={5}>
-                            Playground
-                        </TooltipContent>
-                    </Tooltip>
-
-                    <Tooltip>
-                        <TooltipTrigger asChild>
-                            <Button
-                                variant="ghost"
-                                size="icon"
-                                className="rounded-lg"
-                                aria-label="Models"
-                            >
-                                <Bot className="size-5"/>
-                            </Button>
-                        </TooltipTrigger>
-                        <TooltipContent side="right" sideOffset={5}>
-                            Models
-                        </TooltipContent>
-                    </Tooltip>
-
-                    <Tooltip>
-                        <TooltipTrigger asChild>
-                            <Button
-                                variant="ghost"
-                                size="icon"
-                                className="rounded-lg"
-                                aria-label="API"
-                            >
-                                <Code2 className="size-5"/>
-                            </Button>
-                        </TooltipTrigger>
-                        <TooltipContent side="right" sideOffset={5}>
-                            API
-                        </TooltipContent>
-                    </Tooltip>
-
-                    <Tooltip>
-                        <TooltipTrigger asChild>
-                            <Button
-                                variant="ghost"
-                                size="icon"
-                                className="rounded-lg"
-                                aria-label="Documentation"
-                            >
-                                <Book className="size-5"/>
-                            </Button>
-                        </TooltipTrigger>
-                        <TooltipContent side="right" sideOffset={5}>
-                            Documentation
-                        </TooltipContent>
-                    </Tooltip>
-
-                    <Tooltip>
-                        <TooltipTrigger asChild>
-                            <Button
-                                variant="ghost"
-                                size="icon"
-                                className="rounded-lg"
-                                aria-label="Settings"
-                            >
-                                <Settings2 className="size-5"/>
-                            </Button>
-                        </TooltipTrigger>
-                        <TooltipContent side="right" sideOffset={5}>
-                            Settings
-                        </TooltipContent>
-                    </Tooltip>
-                </nav>
-
-                <nav className="mt-auto grid gap-1 p-2">
-                    <Tooltip>
-                        <TooltipTrigger asChild>
-                            <Button
-                                variant="ghost"
-                                size="icon"
-                                className="mt-auto rounded-lg"
-                                aria-label="Help"
-                            >
-                                <LifeBuoy className="size-5"/>
-                            </Button>
-                        </TooltipTrigger>
-                        <TooltipContent side="right" sideOffset={5}>
-                            Help
-                        </TooltipContent>
-                    </Tooltip>
-
-                    <Tooltip>
-                        <TooltipTrigger asChild>
-                            <Button
-                                variant="ghost"
-                                size="icon"
-                                className="mt-auto rounded-lg"
-                                aria-label="Account"
-                            >
-                                <SquareUser className="size-5"/>
-                            </Button>
-                        </TooltipTrigger>
-                        <TooltipContent side="right" sideOffset={5}>
-                            Account
-                        </TooltipContent>
-                    </Tooltip>
-                </nav>
-            </aside>
-
-            <div className="flex flex-col">
-
-                <header className="sticky top-0 z-10 flex h-[53px] items-center gap-1 border-b bg-background px-4">
-                    <h1 className="text-xl font-semibold">ApolloLink</h1>
-                    <div className="ml-auto gap-1.5 text-sm">
-                        <ModeToggle></ModeToggle>
-                    </div>
-                </header>
-
-                <main className="grid flex-1 gap-4 overflow-auto p-4 grid-cols-3 ">
-
-                    <div
-                        className="relative flex h-full min-h-[50vh] flex-col rounded-xl bg-muted/50 p-4 col-span-3">
-                    </div>
-
-                </main>
-
             </div>
-        </div>
+        </TooltipProvider>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import {Dashboard} from "@/Dasboard.tsx";
-import {TooltipProvider} from "@/components/ui/tooltip.tsx";
 import {ThemeProvider} from "@/components/theme-provider.tsx";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <ThemeProvider>
-            <TooltipProvider>
-                <Dashboard/>
-            </TooltipProvider>
+            <Dashboard/>
         </ThemeProvider>
     </React.StrictMode>,
 )
