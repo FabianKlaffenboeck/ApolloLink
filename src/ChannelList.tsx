5ec58fe94e163a6eeed2d9b0411e995eb8281140
Fabian Klaffenböck <fabian@klaffenboeck.eu>
2024-10-23 19:53:07 +0200

    code cleanup

--- src/ChannelList.tsx
+++ src/ChannelList.tsx
@@ -1,16 +1,8 @@
 import * as React from "react"
 import {Button} from "@/components/ui/button"
 import {Input} from "@/components/ui/input"
-import {CaretSortIcon, DotsHorizontalIcon,} from "@radix-ui/react-icons"
+import {CaretSortIcon} from "@radix-ui/react-icons"
 import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow,} from "@/components/ui/table"
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
 import {
     ColumnDef,
     ColumnFiltersState,
@@ -80,31 +72,6 @@ export const columns: ColumnDef<Payment>[] = [
             }).format(amount)
 
             return <div className="text-right font-medium">{formatted}</div>
-        },
-    },
-    {
-        id: "actions", enableHiding: false, cell: ({row}) => {
-            const payment = row.original
-
-            return (<DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                    <Button variant="ghost" className="h-8 w-8 p-0">
-                        <span className="sr-only">Open menu</span>
-                        <DotsHorizontalIcon className="h-4 w-4"/>
-                    </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent align="end">
-                    <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                    <DropdownMenuItem
-                        onClick={() => navigator.clipboard.writeText(payment.id)}
-                    >
-                        Copy payment ID
-                    </DropdownMenuItem>
-                    <DropdownMenuSeparator/>
-                    <DropdownMenuItem>View customer</DropdownMenuItem>
-                    <DropdownMenuItem>View payment details</DropdownMenuItem>
-                </DropdownMenuContent>
-            </DropdownMenu>)
         },
     }
 ]
