165b1c2d9671ce3c2cac0b3a2f4ca73f17921273
Fabian Klaffenböck <fabian@klaffenboeck.eu>
2024-10-23 17:57:34 +0200

    removed selection

--- src/ChannelList.tsx
+++ src/ChannelList.tsx
@@ -1,6 +1,5 @@
 import * as React from "react"
 import {Button} from "@/components/ui/button"
-import {Checkbox} from "@/components/ui/checkbox"
 import {Input} from "@/components/ui/input"
 import {CaretSortIcon, DotsHorizontalIcon,} from "@radix-ui/react-icons"
 import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow,} from "@/components/ui/table"
@@ -54,72 +53,66 @@ export type Payment = {
     email: string
 }
 
-export const columns: ColumnDef<Payment>[] = [{
-    id: "select", header: ({table}) => (<Checkbox
-        checked={table.getIsAllPageRowsSelected() || (table.getIsSomePageRowsSelected() && "indeterminate")}
-        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-        aria-label="Select all"
-    />), cell: ({row}) => (<Checkbox
-        checked={row.getIsSelected()}
-        onCheckedChange={(value) => row.toggleSelected(!!value)}
-        aria-label="Select row"
-    />), enableSorting: false, enableHiding: false,
-}, {
-    accessorKey: "status",
-    header: "Status",
-    cell: ({row}) => (<div className="capitalize">{row.getValue("status")}</div>),
-}, {
-    accessorKey: "email", header: ({column}) => {
-        return (<Button
-            variant="ghost"
-            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-            Email
-            <CaretSortIcon className="ml-2 h-4 w-4"/>
-        </Button>)
-    }, cell: ({row}) => <div className="lowercase">{row.getValue("email")}</div>,
-}, {
-    accessorKey: "amount", header: () => <div className="text-right">Amount</div>, cell: ({row}) => {
-        const amount = parseFloat(row.getValue("amount"))
-
-        // Format the amount as a dollar amount
-        const formatted = new Intl.NumberFormat("en-US", {
-            style: "currency", currency: "USD",
-        }).format(amount)
-
-        return <div className="text-right font-medium">{formatted}</div>
+export const columns: ColumnDef<Payment>[] = [
+    {
+        accessorKey: "status",
+        header: "Status",
+        cell: ({row}) => (<div className="capitalize">{row.getValue("status")}</div>),
     },
-}, {
-    id: "actions", enableHiding: false, cell: ({row}) => {
-        const payment = row.original
-
-        return (<DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                <Button variant="ghost" className="h-8 w-8 p-0">
-                    <span className="sr-only">Open menu</span>
-                    <DotsHorizontalIcon className="h-4 w-4"/>
-                </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-                <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                <DropdownMenuItem
-                    onClick={() => navigator.clipboard.writeText(payment.id)}
-                >
-                    Copy payment ID
-                </DropdownMenuItem>
-                <DropdownMenuSeparator/>
-                <DropdownMenuItem>View customer</DropdownMenuItem>
-                <DropdownMenuItem>View payment details</DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>)
+    {
+        accessorKey: "email", header: ({column}) => {
+            return (<Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+            >
+                Email
+                <CaretSortIcon className="ml-2 h-4 w-4"/>
+            </Button>)
+        }, cell: ({row}) => <div className="lowercase">{row.getValue("email")}</div>,
     },
-},]
+    {
+        accessorKey: "amount", header: () => <div className="text-right">Amount</div>, cell: ({row}) => {
+            const amount = parseFloat(row.getValue("amount"))
+
+            // Format the amount as a dollar amount
+            const formatted = new Intl.NumberFormat("en-US", {
+                style: "currency", currency: "USD",
+            }).format(amount)
+
+            return <div className="text-right font-medium">{formatted}</div>
+        },
+    },
+    {
+        id: "actions", enableHiding: false, cell: ({row}) => {
+            const payment = row.original
+
+            return (<DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant="ghost" className="h-8 w-8 p-0">
+                        <span className="sr-only">Open menu</span>
+                        <DotsHorizontalIcon className="h-4 w-4"/>
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                    <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                    <DropdownMenuItem
+                        onClick={() => navigator.clipboard.writeText(payment.id)}
+                    >
+                        Copy payment ID
+                    </DropdownMenuItem>
+                    <DropdownMenuSeparator/>
+                    <DropdownMenuItem>View customer</DropdownMenuItem>
+                    <DropdownMenuItem>View payment details</DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>)
+        },
+    }
+]
 
 export function ChannelList() {
     const [sorting, setSorting] = React.useState<SortingState>([])
     const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>([])
     const [columnVisibility, setColumnVisibility] = React.useState<VisibilityState>({})
-    const [rowSelection, setRowSelection] = React.useState({})
 
     const table = useReactTable({
         data,
@@ -132,8 +125,7 @@ export function ChannelList() {
         getSortedRowModel: getSortedRowModel(),
         getFilteredRowModel: getFilteredRowModel(),
         onColumnVisibilityChange: setColumnVisibility,
-        onRowSelectionChange: setRowSelection,
-        state: {sorting, columnFilters, columnVisibility, rowSelection},
+        state: {sorting, columnFilters, columnVisibility},
     })
 
     return (<div className="w-full">
@@ -158,7 +150,7 @@ export function ChannelList() {
                 </TableHeader>
                 <TableBody>
                     {table.getRowModel().rows?.length ? (table.getRowModel().rows.map((row) => (
-                        <TableRow key={row.id} data-state={row.getIsSelected() && "selected"}>
+                        <TableRow key={row.id}>
                             {row.getVisibleCells().map((cell) => (<TableCell key={cell.id}>
                                 {flexRender(cell.column.columnDef.cell, cell.getContext())}
                             </TableCell>))}
@@ -172,10 +164,6 @@ export function ChannelList() {
         </div>
 
         <div className="flex items-center justify-end space-x-2 py-4">
-            <div className="flex-1 text-sm text-muted-foreground">
-                {table.getFilteredSelectedRowModel().rows.length} of{" "}
-                {table.getFilteredRowModel().rows.length} row(s) selected.
-            </div>
             <div className="space-x-2">
                 <Button variant="outline" size="sm" onClick={() => table.previousPage()}
                         disabled={!table.getCanPreviousPage()}>
